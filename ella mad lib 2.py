737346f4096cb29f73e07815604cfac5f1a48850
elmoore <elmoore@gcds.net>
2017-11-09 09:05:13 -0500

    Initial Commit

---
+++ ella mad lib 2.py
@@ -0,0 +1,29 @@
+
+print("Write a noun")
+noun1 = input()
+
+print("write an adjective")
+adjective1 = input()
+
+print("Write a country")
+country1 = input()
+
+print("Write a time period in history")
+timeperiod1 = input()
+
+
+
+
+
+
+
+
+
+### MADLIB ###
+print("Did you know that " + noun1 + " has a very " + adjective1 + " history in " + country1 + "?")
+print("They have been around since before " + timeperiod1 + "!")
+print("They were invented when a " + noun2 + " had gone to the " + noun3 + "."
+print("There as she was " + verb + " to get pick out a " + noun4 + ", she had seen some " + material1 + ".")
+print("She had a great idea! This could be used to make a " + noun1 + "!")
+print("Quickly, she picked out some " + noun5 + ", some " + noun6 + ", and a few " + noun7 + ".")
+
