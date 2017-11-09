fce77cd8f515be2169c7ea261dcb3472d7647ba5
elmoore <elmoore@gcds.net>
2017-11-09 09:25:15 -0500

    Added time and grammatical corrections

--- ella mad lib 2.py
+++ ella mad lib 2.py
@@ -1,3 +1,6 @@
+import time
+
+
 
 print("Write a noun")
 noun1 = input()
@@ -11,7 +14,29 @@ country1 = input()
 print("Write a time period in history")
 timeperiod1 = input()
 
+print("Write a plural noun")
+noun2 = input()
 
+print("Write another noun")
+noun3 = input()
+
+print("Next, write a verb")
+verb1 = input()
+
+print("Write a noun")
+noun4 = input()
+
+print("Write a type of material")
+material1 = input()
+
+print("Write a noun")
+noun5 = input()
+
+print("Write a second noun")
+noun6 = input()
+
+print("Last noun! This time, make it plural")
+noun7 = input()
 
 
 
@@ -20,10 +45,13 @@ timeperiod1 = input()
 
 
 ### MADLIB ###
-print("Did you know that " + noun1 + " has a very " + adjective1 + " history in " + country1 + "?")
+print("Did you know that " + noun1 + " have a very " + adjective1 + " history in " + country1 + "?")
 print("They have been around since before " + timeperiod1 + "!")
-print("They were invented when a " + noun2 + " had gone to the " + noun3 + "."
-print("There as she was " + verb + " to get pick out a " + noun4 + ", she had seen some " + material1 + ".")
+print("They were invented when a " + noun2 + " had gone to the " + noun3 + ".")
+print("There as she was " + verb1 + " to get pick out a " + noun4 + ", she had seen some " + material1 + ".")
 print("She had a great idea! This could be used to make a " + noun1 + "!")
 print("Quickly, she picked out some " + noun5 + ", some " + noun6 + ", and a few " + noun7 + ".")
+print("Then, she put them all together to create this amazing invention!")
 
+
+time.sleep(60)
